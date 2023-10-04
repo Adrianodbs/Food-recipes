11eb92a588486bac653dd75d0a501f26bd8bfc62
Adriano <adriano_alves_2008@hotmail.com>
2023-10-04 11:45:50 -0300

    Feat; create page recipes by letter

--- src/pages/RecipesbyLetters/index.tsx
+++ src/pages/RecipesbyLetters/index.tsx
@@ -1,3 +1,59 @@
+import { useState } from 'react'
+import { alphabet } from '../../utils/alphabet'
+import * as C from './style'
+import apiData from '../../services/api'
+import MealProps from '../../interfaces/MealProps'
+import FoodCard from '../../components/FoodCard'
+
 export default function RecipesbyLetters() {
-  return <div>RecipesbyLetters</div>
+  const [recipes, setRecipes] = useState<MealProps[]>([])
+
+  const fetchRecipesByLetter = async (letter: string) => {
+    try {
+      const response = await apiData.get(`search.php?f=${letter}`)
+      const getMeals: MealProps[] =
+        response.data.meals?.map((meal: any) => ({
+          idMeal: meal.idMeal,
+          strMealThumb: meal.strMealThumb,
+          strMeal: meal.strMeal,
+          strArea: meal.strArea,
+          youtubeUrl: meal.strYoutube
+        })) ?? []
+      setRecipes(getMeals)
+    } catch (error) {
+      console.error('Error fetching recipes: ', error)
+    }
+  }
+
+  const handleLetterClick = (letter: string) => {
+    fetchRecipesByLetter(letter)
+  }
+
+  return (
+    <C.Container>
+      <h1>Encontre o prato pela letra</h1>
+      <C.LetterList>
+        {alphabet.map(l => (
+          <li key={l} onClick={() => handleLetterClick(l)}>
+            {l}
+          </li>
+        ))}
+      </C.LetterList>
+      <C.Content>
+        {recipes.length > 0 ? (
+          recipes.map(meal => (
+            <FoodCard
+              key={meal.idMeal}
+              image={meal.strMealThumb}
+              title={meal.strMeal}
+              nationality={meal.strArea}
+              youtubeUrl={meal.youtubeUrl}
+            />
+          ))
+        ) : (
+          <p>Nenhum pra encontrado com essa letra</p>
+        )}
+      </C.Content>
+    </C.Container>
+  )
 }
--- src/pages/RecipesbyLetters/style.ts
+++ src/pages/RecipesbyLetters/style.ts
@@ -1,3 +1,40 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: #F3F4F6;
+  min-height: calc(100vh - 48px);
+
+  h1{
+    color: var(--primary);
+    margin: 20px 0 10px;
+  }
+`
+
+export const LetterList = styled.ul`
+  display: flex;
+  list-style: none;
+  gap: 5px;
+
+  li{
+    cursor: pointer;
+    color: var(--secondary);
+    font-size: 18px;
+
+    &:hover{
+      color: var(--primary);
+      transform: scale(1.15);
+    }
+  }
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 30px;
+`
