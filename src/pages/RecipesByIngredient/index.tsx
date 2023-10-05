f9b31f2e205cb4cf027d13d896e59ae52ff2ef16
Adriano <adriano_alves_2008@hotmail.com>
2023-10-05 14:02:37 -0300

    Feat: create ingredient page

--- src/pages/RecipesByIngredient/index.tsx
+++ src/pages/RecipesByIngredient/index.tsx
@@ -1,9 +1,111 @@
+import FoodCard from '../../components/FoodCard'
+import Search from '../../components/Search'
+import MealProps from '../../interfaces/MealProps'
+import apiData from '../../services/api'
 import * as C from './style'
+import { useState, useEffect } from 'react'
 
 export default function RecipesByIngredient() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [searchResults, setSearchResults] = useState<MealProps[]>([])
+  const [allIngredients, setAllIngredients] = useState<string[]>([])
+
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      try {
+        const response = await apiData.get('list.php?i=list')
+
+        const ingredients: string[] =
+          response.data.meals?.map(
+            (ingredient: any) => ingredient.strIngredient
+          ) ?? []
+        setAllIngredients(ingredients)
+      } catch (error) {
+        console.error('Erro ao buscar ingredientes:', error)
+      }
+    }
+
+    fetchIngredients()
+  }, [])
+
+  const handleSearchSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault()
+    try {
+      const response = await apiData.get('filter.php', {
+        params: {
+          i: searchTerm,
+          apikey: '1'
+        }
+      })
+      const getMeals: MealProps[] =
+        response.data.meals?.map((meal: any) => ({
+          idMeal: meal.idMeal,
+          strMealThumb: meal.strMealThumb,
+          strMeal: meal.strMeal,
+          strArea: meal.strArea,
+          youtubeUrl: meal.strYoutube
+        })) ?? []
+
+      setSearchResults(getMeals)
+    } catch (error) {
+      console.error('Erro ao buscar refeições:', error)
+    }
+  }
+
+  const handleIngredientClick = async (ingredient: string) => {
+    try {
+      const response = await apiData.get('filter.php', {
+        params: {
+          i: ingredient,
+          apikey: '1'
+        }
+      })
+      const getMeals: MealProps[] =
+        response.data.meals?.map((meal: any) => ({
+          idMeal: meal.idMeal,
+          strMealThumb: meal.strMealThumb,
+          strMeal: meal.strMeal,
+          strArea: meal.strArea,
+          youtubeUrl: meal.strYoutube
+        })) ?? []
+
+      setSearchResults(getMeals)
+    } catch (error) {
+      console.error('Erro ao buscar refeições:', error)
+    }
+  }
   return (
     <C.Container>
       <h1>Receitas por ingredientes</h1>
+      <Search
+        submit={handleSearchSubmit}
+        placeholder="Digite o nome do ingrediente"
+        value={searchTerm}
+        onChange={event => setSearchTerm(event.target.value)}
+      />
+      <C.FoodList>
+        {searchResults.length > 0 ? (
+          searchResults.map(meal => (
+            <FoodCard
+              key={meal.idMeal}
+              image={meal.strMealThumb}
+              title={meal.strMeal}
+              nationality={meal.strArea}
+              youtubeUrl={meal.youtubeUrl}
+            />
+          ))
+        ) : (
+          <C.IngredientList>
+            {allIngredients.map((ingredient, index) => (
+              <li key={index} onClick={() => handleIngredientClick(ingredient)}>
+                {ingredient}
+              </li>
+            ))}
+          </C.IngredientList>
+        )}
+      </C.FoodList>
     </C.Container>
   )
 }
--- src/pages/RecipesByIngredient/style.ts
+++ src/pages/RecipesByIngredient/style.ts
@@ -1,3 +1,47 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: #F3F4F6;
+  min-height: calc(100vh - 48px);
+
+  h1{
+    color: var(--primary);
+    margin: 20px 0 10px;
+  }
+`
+
+export const FoodList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+`
+
+export const IngredientList = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 40px;
+  padding: 10px 20px;
+  max-width: 900px;
+  width: 90%;
+
+  li{
+    cursor: pointer;
+    width: 220px;
+    background-color: #fff;
+    padding: 16px 10px;
+    border-radius: 4px;
+    font-weight: 600;
+    
+    &:hover{
+      transform: scale(1.08);
+    }
+  }
+`
